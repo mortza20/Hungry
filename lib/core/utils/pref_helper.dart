ff4d1da80377f0fe362ab696732b3b0235e91203
mortza20 <mortza20@gmail.com>
2025-11-17 14:56:15 +0300

    create the api empty api files only in the core folder

---
+++ lib/core/utils/pref_helper.dart
@@ -0,0 +1,10 @@
+class ApiError {
+  final String message;
+  final int? statusCode;
+  ApiError({required this.message, this.statusCode});
+
+  @override
+  String toString() {
+    return "Eerrors:$message (StatesCode is$statusCode";
+  }
+}
